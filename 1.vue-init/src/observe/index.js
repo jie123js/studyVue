7a7b60574f1a612b7dd83bee1309e02aee97ada4
jie123js <1178232566@qq.com>
2021-09-03 15:32:35 +0800

    up

--- 1.vue-init/src/observe/index.js
+++ 1.vue-init/src/observe/index.js
@@ -33,11 +33,12 @@ class Observer {
 // vue2 应用了defineProperty需要一加载的时候 就进行递归操作，所以好性能，如果层次过深也会浪费性能
 // 1.性能优化的原则：
 // 1) 不要把所有的数据都放在data中，因为所有的数据都会增加get和set
-// 2) 不要写数据的时候 层次过深， 尽量扁平化数据 
-// 3) 不要频繁获取数据
+// 2) 不要写数据的时候 层次过深， 尽量扁平化数据  例如 a:{b:{c:{}}}
+// 3) 不要频繁获取数据 例如this.xxxx++ 加个10W次这种
 // 4) 如果数据不需要响应式 可以使用Object.freeze 冻结属性 
 function defineReactive(obj,key,value){ // vue2 慢的原因 主要在这个方法中
     observe(value); // 递归进行观测数据，不管有多少层 我都进行defineProperty
+    //!这里也不能直接使用obj[key]这样会死循环 return obj[key]的话相当于又获取了值 又get
     Object.defineProperty(obj,key,{
         get(){ // 后续会有很多逻辑
             return value; // 闭包，次此value 会像上层的value进行查找
